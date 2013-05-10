49014761a7813c4cc52d79b57f8d0aeba892f5e5
Justin Robbins <justinhrobbins@gmail.com>
2013-05-10 07:13:46 -0600

    added test cases to increase unit test coverage

--- FlashCards_Service_SpringData/src/main/java/org/robbins/flashcards/service/springdata/base/GenericCrudServiceImpl.java
+++ FlashCards_Service_SpringData/src/main/java/org/robbins/flashcards/service/springdata/base/GenericCrudServiceImpl.java
@@ -13,20 +13,12 @@ public abstract class GenericCrudServiceImpl<T, ID extends Serializable> impleme
 		return getRepository().save(entity);
 	}
 
-	public Iterable<T> save(Iterable<T> entities) {
-		return getRepository().save(entities);
-	}
-
 	public T findOne(ID id) {
 		return getRepository().findOne(id);
 	}
 
 	public boolean exists(ID id) {
 		return getRepository().exists(id);
-	}
-
-	public Iterable<T> findAll() {
-		return getRepository().findAll();
 	}
 
 	public Iterable<T> findAll(Iterable<ID> ids) {
--- FlashCards_Service_SpringData/src/test/java/org/robbins/flashcards/service/springdata/base/GenericCrudServiceImplUT.java
+++ FlashCards_Service_SpringData/src/test/java/org/robbins/flashcards/service/springdata/base/GenericCrudServiceImplUT.java
@@ -2,8 +2,12 @@ package org.robbins.flashcards.service.springdata.base;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -28,12 +32,25 @@ public class GenericCrudServiceImplUT extends BaseMockingTest {
 	@Mock
 	Tag tag;
 	
+	@Mock
+	List<Tag> tags;
+	
 	@Before
 	public void before() {
 		tagService = new TagServiceImpl();
 		ReflectionTestUtils.setField(tagService, "repository", repository);		
 	}
 
+	@Test
+	public void save() {
+		when(repository.save(tag)).thenReturn(tag);
+		
+		Tag result = tagService.save(tag);
+		
+		verify(repository, Mockito.times(1)).save(tag);
+		assertThat(result, is(Tag.class));		
+	}
+	
 	@Test
 	public void findOne() {
 		when(repository.findOne(Mockito.anyLong())).thenReturn(tag);
@@ -42,5 +59,27 @@ public class GenericCrudServiceImplUT extends BaseMockingTest {
 		
 		verify(repository, Mockito.times(1)).findOne(1L);
 		assertThat(result, is(Tag.class));
+	}
+
+	@Test
+	public void exists() {
+		when(repository.exists(Mockito.anyLong())).thenReturn(Boolean.TRUE);
+		
+		Boolean result = tagService.exists(1L);
+		
+		verify(repository, Mockito.times(1)).exists(1L);
+		assertThat(result, is(Boolean.class));
+		assertTrue(result.booleanValue());
+	}
+	
+	@Test
+	public void findAllIterable() {
+		List<Long> ids = new ArrayList<Long>();
+		when(repository.findAll(ids)).thenReturn(tags);
+		
+		Iterable<Tag> results = tagService.findAll(ids);
+		
+		verify(repository, Mockito.times(1)).findAll(ids);
+		assertThat(results, is(List.class));
 	}
 }
