c0566f44d1aecc80b4461f80f98d6f69905c8095
Justin Robbins <justin.robbins@hybris.com>
2014-02-28 12:04:11 -0700

    #125

--- FlashCards_AngularJS/src/main/webapp/app/js/controllers.js
+++ FlashCards_AngularJS/src/main/webapp/app/js/controllers.js
@@ -7,7 +7,7 @@ var flashCardsAppControllers = angular.module('flashCardsAppControllers', []);
 flashCardsAppControllers.controller('TagListCtrl', ['$scope', 'Tag',
     function ($scope, Tag) {
         $scope.tags = Tag.query();
-        $scope.orderProp = 'name';
+        $scope.orderProp = 'id';
     }]);
 
 flashCardsAppControllers.controller('TagDetailCtrl', ['$scope', '$routeParams', 'Tag',
