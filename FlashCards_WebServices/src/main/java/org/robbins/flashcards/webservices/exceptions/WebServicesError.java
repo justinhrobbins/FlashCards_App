874742f99767c316603c91ee2a9dc12766158ce8
Justin Robbins | hybris <justin.robbins@hybris.com>
2015-01-04 08:57:58 -0700

    fix error dto name

--- FlashCards_WebServices/src/main/java/org/robbins/flashcards/webservices/exceptions/WebServicesError.java
+++ FlashCards_WebServices/src/main/java/org/robbins/flashcards/webservices/exceptions/WebServicesError.java
@@ -5,7 +5,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement(name = "tag")
+@XmlRootElement(name = "error")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class WebServicesError {
 
